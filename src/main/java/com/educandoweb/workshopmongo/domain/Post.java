f6d9db0fee8d392bfa6f21e0d9082ca0730f8b72
Guilherme Cardozo <guilhermecardozo91@gmail.com>
2022-11-09 14:52:17 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/educandoweb/workshopmongo/domain/Post.java
+++ src/main/java/com/educandoweb/workshopmongo/domain/Post.java
@@ -1,4 +1,4 @@
-package com.educandoweb.workshopmongo.dto;
+package com.educandoweb.workshopmongo.domain;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -6,23 +6,23 @@ import java.util.Date;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import com.educandoweb.workshopmongo.domain.User;
+import com.educandoweb.workshopmongo.dto.AuthorDTO;
 
 @Document
 public class Post implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	private String id;
 	private Date date;
 	private String title;
 	private String body;
-	private User author;
-	
+	private AuthorDTO author;
+
 	public Post() {
 	}
 
-	public Post(String id, Date date, String title, String body, User author) {
+	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		super();
 		this.id = id;
 		this.date = date;
@@ -63,11 +63,11 @@ public class Post implements Serializable {
 		this.body = body;
 	}
 
-	public User getAuthor() {
+	public AuthorDTO getAuthor() {
 		return author;
 	}
 
-	public void setAuthor(User author) {
+	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
 
--- src/main/java/com/educandoweb/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/educandoweb/workshopmongo/repository/PostRepository.java
@@ -3,7 +3,7 @@ package com.educandoweb.workshopmongo.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.educandoweb.workshopmongo.dto.Post;
+import com.educandoweb.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
